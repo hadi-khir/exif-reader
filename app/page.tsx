b13f0a46a88129baf13d94bd306253518d95162c
Hadi Khir <khir.hadi@gmail.com>
2024-09-27 16:27:41 +0000

    adding exif reader raw functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import exif from "exif-js";
 import { Card, CardContent } from "@/components/ui/card";
 import { ChangeEventHandler, useState } from "react";
 
@@ -7,6 +8,29 @@ export default function Home() {
 
   const [uploadedImage, setUploadedImage] = useState<File>();
   const [imagePreviewUrl, setImagePreviewUrl] = useState("");
+  const [exifData, setExifData] = useState(null);
+
+  const extractExifData = (file: File) => {
+
+
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        const image = new Image();
+        image.src = event.target?.result as string;
+        image.onload = function () {
+          
+          //@ts-ignore
+          exif.getData(image, () => {
+            const exifData = exif.getAllTags(this);
+            resolve(exifData);
+          });
+        };
+      }
+      reader.onerror = reject;
+      reader.readAsDataURL(file);
+    })
+  }
 
   const fileToDataString = (file: File) => {
     return new Promise<string>((resolve, reject) => {
@@ -29,11 +53,17 @@ export default function Home() {
       try {
         const imgUrl = await fileToDataString(file);
         setImagePreviewUrl(imgUrl);
+
+        const exif: any = await extractExifData(file);
+        setExifData(exif);
       } catch (error) {
         console.log(error);
       }
     }
   }
+
+  console.log(exifData);
+
 
   return (
     <div className="container mx-auto py-4">
@@ -51,6 +81,16 @@ export default function Home() {
           {imagePreviewUrl && (
             <div className="mt-4">
               <img src={imagePreviewUrl} alt="Uploaded" className="max-w-full h-auto mx-auto" />
+            </div>
+          )}
+
+
+          {exifData && (
+            <div className="mt-4 p-4 bg-gray-100 rounded-lg">
+              <h2 className="text-xl font-semibold">Image EXIF Data</h2>
+              <pre className="whitespace-pre-wrap text-sm">
+                {JSON.stringify(exifData, null, 2)}
+              </pre>
             </div>
           )}
         </CardContent>
