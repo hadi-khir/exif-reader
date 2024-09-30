a90158f5c5d5ca8902537cc593d51e0800ab93e0
Hadi Khir <khir.hadi@gmail.com>
2024-09-30 12:39:39 +0000

    adding upload thing api

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,16 @@
+import { createUploadthing, FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+    productImage: f({
+        image: { maxFileSize: "4MB", maxFileCount: 1 },
+    }).onUploadComplete(async ({ metadata, file }) => {
+
+        const userId = (metadata as any).userId;
+        console.log("Upload complete for userId:", userId);
+        console.log("file url", file.url);
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
